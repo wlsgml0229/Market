43ab9afb356c61891f82e62eb847eaf8378faee9
jinhee kim <wlsgml0229@naver.com>
2023-07-09 15:51:46 +0900

    Design: Create Heading, Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+interface ContainerProps {
+  children: React.ReactNode
+}
+const Container = ({ children }: ContainerProps) => {
+  return (
+    <div className="max-w-[2529px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 py-6">
+      {children}
+    </div>
+  )
+}
+
+export default Container
